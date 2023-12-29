5a33a1da50530a726e665841c0eb531a9404fbc4
Eftekhar Alam <eftialam@hotmail.com>
2023-12-30 00:04:34 +0600

    views publish

--- src/ContactServiceProvider.php
+++ src/ContactServiceProvider.php
@@ -9,7 +9,11 @@ class ContactServiceProvider extends ServiceProvider{
         $this->loadMigrationsFrom(__DIR__.'/database/migrations');
         $this->mergeConfigFrom(__DIR__.'/config/contact.php','contact');
 
-        $this->publishes([__DIR__.'/config/contact.php' => config_path('contact.php')]);
+        $this->publishes([
+            __DIR__.'/config/contact.php' => config_path('contact.php'),
+            __DIR__.'/views' => resource_path('views/vendor/contact'),
+
+        ]);
     }
     public function register(){
 
